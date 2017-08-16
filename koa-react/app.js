91013e783dc54ab87104fe383e9567cfb9ea45ce
laohuimin <fishlao@126.com>
2017-08-16 20:48:20 +0800

    MusicCloud
    构建框架中.....

---
+++ koa-react/app.js
@@ -0,0 +1,30 @@
+const Koa = require('koa');
+const app = new Koa();
+
+// x-response-time
+
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  ctx.set('X-Response-Time', `${ms}ms`);
+  console.log("here is x-response-time")
+});
+
+// logger
+
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  console.log(`${ctx.method} ${ctx.url} - ${ms}`);
+  console.log("here is logger")
+});
+
+// response
+
+app.use(async ctx => {
+  ctx.body = 'Hello World';
+});
+
+app.listen(3000);
