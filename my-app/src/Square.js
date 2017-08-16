424bf273515c2d826ca1d4ee0218b6451cc700d3
laohuimin <fishlao@126.com>
2017-08-16 20:47:46 +0800

    react小游戏

---
+++ my-app/src/Square.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import './index.css';
+
+function Square(props) {
+    return (
+      <button className="square" onClick={() => props.onClick()}>
+        {props.value}
+      </button>
+    );
+}
+export default Square;
